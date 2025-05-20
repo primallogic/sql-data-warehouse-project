2da35e689e824053a87a76311f0e05e056e75112
primallogic <131926528+primallogic@users.noreply.github.com>
2025-05-20 10:40:24 -0400

    Update and rename bulk_insert_source_crm.sql to bulk_insert_source_crm_erp.sql

--- scripts/bulk_insert_source_crm.sql
+++
@@ -1,7 +0,0 @@
-BULK INSERT bronze.crm_cust_info
-FROM 'C:\Users\Primal Logic HQ\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
