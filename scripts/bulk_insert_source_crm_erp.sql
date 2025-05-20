f8d9d2537749cbb24a4448b7514e75030bf9c811
primallogic <131926528+primallogic@users.noreply.github.com>
2025-05-20 10:57:20 -0400

    Update bulk_insert_source_crm_erp.sql

--- scripts/bulk_insert_source_crm_erp.sql
+++ scripts/bulk_insert_source_crm_erp.sql
@@ -1,108 +1,93 @@
--- FULL LOAD into the bronze layer by truncating before inserting new information
-TRUNCATE TABLE bronze.crm_cust_info
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	PRINT '========================================================';
+	PRINT 'Loading Bronze Layer';
+	PRINT '========================================================';
 
-BULK INSERT bronze.crm_cust_info
-FROM 'C:\Users\Primal Logic HQ\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	PRINT '--------------------------------------------------------';
+	PRINT 'Loading CRM Tables';
+	PRINT '--------------------------------------------------------';
+	
+	PRINT '>> Truncating Table: bronze.crm_cust_info';
+	TRUNCATE TABLE bronze.crm_cust_info
 
-
-SELECT * FROM bronze.crm_cust_info
-
-SELECT COUNT(*) FROM bronze.crm_cust_info
-
-
-
-
--- FULL LOAD into the bronze layer by truncating before inserting new information
-TRUNCATE TABLE bronze.crm_prd_info
-
-BULK INSERT bronze.crm_prd_info
-FROM 'C:\Users\Primal Logic HQ\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	PRINT '>> Inserting Data Into Table: bronze.crm_cust_info';
+	BULK INSERT bronze.crm_cust_info
+	FROM 'C:\Users\Primal Logic HQ\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
 
-SELECT * FROM bronze.crm_prd_info
+	PRINT '>> Truncating Table: bronze.crm_prd_info';
+	TRUNCATE TABLE bronze.crm_prd_info
 
-SELECT COUNT(*) FROM bronze.crm_prd_info
-
-
--- FULL LOAD into the bronze layer by truncating before inserting new information
-TRUNCATE TABLE bronze.crm_sales_details
-
-BULK INSERT bronze.crm_sales_details
-FROM 'C:\Users\Primal Logic HQ\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	PRINT '>> Inserting Data Into Table: bronze.crm_prd_info';
+	BULK INSERT bronze.crm_prd_info
+	FROM 'C:\Users\Primal Logic HQ\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
 
-SELECT * FROM bronze.crm_sales_details
+	PRINT '>> Truncating Table: bronze.crm_sales_details';
+	TRUNCATE TABLE bronze.crm_sales_details
 
-SELECT COUNT(*) FROM bronze.crm_sales_details
+	PRINT '>> Inserting Data Into Table: bronze.crm_sales_details';
+	BULK INSERT bronze.crm_sales_details
+	FROM 'C:\Users\Primal Logic HQ\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
 
 
--- FULL LOAD into the bronze layer by truncating before inserting new information
-TRUNCATE TABLE bronze.erp_cust_az12
+	PRINT '--------------------------------------------------------';
+	PRINT 'Loading ERP Tables';
+	PRINT '--------------------------------------------------------';
+	
+	PRINT '>> Truncating Table: bronze.erp_cust_az12';
+	TRUNCATE TABLE bronze.erp_cust_az12
 
-BULK INSERT bronze.erp_cust_az12
-FROM 'C:\Users\Primal Logic HQ\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	PRINT '>> Inserting Data Into Table: bronze.erp_cust_az12';
+	BULK INSERT bronze.erp_cust_az12
+	FROM 'C:\Users\Primal Logic HQ\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
+	PRINT '>> Truncating Table: bronze.erp_loc_a101';
+	TRUNCATE TABLE bronze.erp_loc_a101
 
-SELECT * FROM bronze.erp_cust_az12
+	PRINT '>> Inserting Data Into Table: bronze.erp_loc_a101';
+	BULK INSERT bronze.erp_loc_a101
 
-SELECT COUNT(*) FROM bronze.erp_cust_az12
+	FROM 'C:\Users\Primal Logic HQ\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
+	PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
+	TRUNCATE TABLE bronze.erp_px_cat_g1v2
 
--- FULL LOAD into the bronze layer by truncating before inserting new information
-TRUNCATE TABLE bronze.erp_loc_a101
+	PRINT '>> Inserting Data Into Table: bronze.erp_px_cat_g1v2';
+	BULK INSERT bronze.erp_px_cat_g1v2
 
-BULK INSERT bronze.erp_loc_a101
+	FROM 'C:\Users\Primal Logic HQ\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
 
-FROM 'C:\Users\Primal Logic HQ\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-
-
-SELECT * FROM bronze.erp_loc_a101
-
-
-SELECT COUNT(*) FROM bronze.erp_loc_a101
-
-
-
--- FULL LOAD into the bronze layer by truncating before inserting new information
-TRUNCATE TABLE bronze.erp_px_cat_g1v2
-
-BULK INSERT bronze.erp_px_cat_g1v2
-
-FROM 'C:\Users\Primal Logic HQ\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-
-
-SELECT * FROM bronze.erp_px_cat_g1v2
-
-
-SELECT COUNT(*) FROM bronze.erp_px_cat_g1v2
+END
