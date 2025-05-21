ef9a0e890e47aa51accb3b5325ad9171f45c661d
primallogic <131926528+primallogic@users.noreply.github.com>
2025-05-21 19:06:11 -0400

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -42,14 +42,7 @@ BEGIN
 	PRINT '>> Truncating Table: silver.crm_cust_info';
 	TRUNCATE TABLE silver.crm_cust_info;
 	PRINT '>> Inserting Data Into: silver.erp_px_cat_g1v2';
-	INSERT INTO silver.erp_px_cat_g1v2
-	(id, cat, subcat, maintenance)
-	SELECT
-	id,
-	cat,
-	subcat,
-	maintenance
-	FROM bronze.erp_px_cat_g1v2
+	cst_marital_status
 	
 	SET @end_time = GETDATE();
 	PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
